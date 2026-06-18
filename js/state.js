aa0ff7b00b84da4a165bf6f153e94f93d971cf12
Max19was <max19ytchannel@gmail.com>
2026-06-18 18:55:33 +0530

    Delete js directory

--- js/state.js
+++
@@ -1,6 +0,0 @@
-// Mutable game state. These values reset during a new match.
-
-let diff=0,youTeamIdx=0,oppTeamIdx=1;
-let questions=[],kick=0,youScore=0,aiScore=0;
-let youResults=[],aiResults=[],aiMisses=[];
-let timerInterval=null,timeLeft=0,totalTime=0,kickStart=0,answered=false;
